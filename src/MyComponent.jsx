30ad7bdc5d9e68201f621f263ca3eb579d9c7bd6
Corey Wofford <msucorey@gmail.com>
2018-09-13 13:20:44 -0400

    Test commit

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export class MyComponent extends React.Component {
   render() {
-    return <div>hey look at me</div>;
+    return <div>hey look at me - i've changed</div>;
   }
 }
 
